edb6276e059b0b3bc91a0ad60568754370ff0664
Proustibat <jennifer.proust@gmail.com>
2017-12-12 12:08:00 +0100

    Deploy to Github Pages

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ rm -rf *.js *.map *.gz *.css *.html
 cp -R dist/. ./
 rm -rf dist
 git commit -am "Deploy to Github Pages"
-git push --force "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+git push --force "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git"
