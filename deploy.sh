ff19644f1f3a066aa48fe6ee259d7cbd16fd258b
Proustibat <jennifer.proust@gmail.com>
2017-12-12 12:08:41 +0100

    Deploy to Github Pages

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ rm -rf *.js *.map *.gz *.css *.html
 cp -R dist/. ./
 rm -rf dist
 git commit -am "Deploy to Github Pages"
-git push --force "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git"
+#git push --force "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git"
+git push --force
